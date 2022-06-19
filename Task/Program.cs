db8a3601bcc49932637d53f34b066393ed9afbb6
YukiShinji <lex.lipov@mail.ru>
2022-06-19 12:17:58 +0300

    Initial SecondArray

--- Task/Program.cs
+++ Task/Program.cs
@@ -1,2 +1,14 @@
 ﻿string[] array1 = new string[] {"23445", "Hi", "Fine", "123", "Supper", "Po"};
 string[] array2 = new string[array1.Length];
+void SecondArray(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
